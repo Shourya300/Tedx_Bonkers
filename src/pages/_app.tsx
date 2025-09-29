c5eb03e780839ba11740759e0bd9194cde581cc0
Shourya300 <shouryasrivastava2006@gmail.com>
2025-09-29 23:16:23 +0530

    HEHE

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,12 @@
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
-import HeaderMob from "@/components/HeaderMob/HeaderMob";
-import { useEffect, useState } from "react";
+import Maintenance from "./index";
 
-function MyApp({ Component, pageProps, router }: AppProps) {
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-
-  useEffect(() => {
-    const checkScreenSize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    checkScreenSize();
-
-    window.addEventListener("resize", checkScreenSize);
-
-    return () => {
-      window.removeEventListener("resize", checkScreenSize);
-    };
-  }, []);
-
-  const showHeaderFooter =
-    router.pathname !== "/" &&
-    router.pathname !== "/page" &&
-    router.pathname !== "/wordle";
-
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider defaultTheme="dark" attribute="class">
-      {showHeaderFooter && (isMobile ? <HeaderMob /> : <Header />)}
-      <Component {...pageProps} />
-      {showHeaderFooter && <Footer />}
+      <Maintenance />
     </ThemeProvider>
   );
 }
