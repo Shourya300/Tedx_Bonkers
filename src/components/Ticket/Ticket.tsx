7991caaa5a2f639542dc7ce7b24c3e1de76fbb43
Arjun-143-Kamath <arjun.kamath24@st.niituniversity.in>
2026-01-22 13:08:41 +0530

    some more commits

--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Mona_Sans } from "next/font/google";
 import fontArray from "./fontArray";
+import FaultyTerminal from "./FaultyTerminal";
 
 const mona = Mona_Sans({
   subsets: ["latin"],
@@ -9,33 +10,34 @@ const mona = Mona_Sans({
 const Ticket = () => {
   const [fonts, setFonts] = useState(mona);
 
-  useEffect(() => {
-    const applyFonts = async () => {
-      let delay = 150;
-      const maxDelay = 200;
-      const increment = 20;
-      for (const font of fontArray) {
-        setFonts(font);
-        await new Promise((resolve) => setTimeout(resolve, delay));
-        delay = Math.min(delay + increment, maxDelay);
-      }
-    };
-
-    applyFonts();
-  }, []); // Removed fontArray from the dependency array
-
   return (
     <div className="everything">
-      <div className="hero h-screen flex items-center justify-center">
+      <div className="hero h-screen flex items-center justify-center relative">
         <div className="absolute inset-0 z-[-1]">
-        </div>
-        <div className=" w-full h-screen lg:text-9xl text-7xl text-center font-bold bg-black bg-opacity-20 backdrop-blur-lg pt-16 drop-shadow-sm flex flex-col items-center justify-center">
-          <div className="lg:text-7xl py-6 text-red-800 text-5xl mt-[-10vh]">
-            Buy
+          <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+
+            <FaultyTerminal
+              scale={2.3}
+              gridMul={[2, 1]}
+              digitSize={1.2}
+              timeScale={0.6}
+              pause={false}
+              scanlineIntensity={0.5}
+              glitchAmount={1}
+              flickerAmount={1}
+              noiseAmp={1}
+              chromaticAberration={0}
+              dither={0}
+              curvature={0.2}
+              tint="#eb0028"
+              mouseReact={true}
+              mouseStrength={0.5}
+              pageLoadAnimation
+              brightness={0.8}
+            />
           </div>
-          <div className={fonts.className}>Tickets</div>
-          <div className="lg:text-4xl py-4 text-3xl">Now</div>
         </div>
+        
       </div>
     </div>
   );
