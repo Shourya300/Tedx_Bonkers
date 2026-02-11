bfd29d9685ded01610b0826ea238516883191a03
Shourya300 <shouryasrivastava2006@gmail.com>
2026-02-11 23:58:52 +0530

    Search scroll fixed

--- src/pages/rewind.tsx
+++ src/pages/rewind.tsx
@@ -60,11 +60,14 @@ export default function RewindPage() {
   useEffect(() => {
     if (searchOpen) {
       document.body.style.overflow = "hidden";
+      document.documentElement.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "";
+      document.documentElement.style.overflow = "";
     }
     return () => {
       document.body.style.overflow = "";
+      document.documentElement.style.overflow = "";
     };
   }, [searchOpen]);
 
